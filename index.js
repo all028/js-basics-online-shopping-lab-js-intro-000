1f3fd628b85031ec8967b2904176a208e5c5c594
al <albliu@email.com>
2019-11-26 18:14:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,6 +63,11 @@ function total() {
 }
 
 function removeFromCart(item) {
+
+  var index = cart.findIndex(x => x.itemName ===item);
+  return index;
+
+  
   for (let i = 0; i < cart.length;i++){
     if (cart[i].itemName == item){
       cart.splice(i,1);
